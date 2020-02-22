8e6f3df8abef91367c0badc52f4fa23507603a0f
Patrick Lam <prof.lam@gmail.com>
2020-02-22 23:21:19 +1300

    provide bash script to download, add buildtype to json

---
+++ shared/bin/download.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+  echo Usage: $0 projects.json
+  exit 1
+fi
+
+INPUT=$1
+for c in $(jq '.[].versions[].source' < "$INPUT"); do
+  /usr/bin/wget -nc `sed -e 's/^"//' -e 's/"$//' <<<"$c"`
+done
+
+for tar in *.tgz *.tar.gz; do
+  tar xzf $tar
+done
+
+for zip in *.zip; do
+  unzip $zip
+done
+
