b0c09bac34f3972e1a39b02cef644cd04847283c
Patrick Lam <prof.lam@gmail.com>
2020-03-05 22:16:59 +1300

    clean.sh now takes an optional target project

--- shared/bin/clean.sh
+++ shared/bin/clean.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
-  echo Usage: $0 projects.json
+if [ "benchmarks" != $(basename $(pwd)) ]; then
+  echo "Must run from 'benchmarks' directory (inside the VM)!"
   exit 1
 fi
 
-if [ "benchmarks" != $(basename $(pwd)) ]; then
-  echo Must run from 'benchmarks' directory!
+if [ -z $1 ]; then
+  echo Usage: $0 ../shared/projects.json
   exit 1
+fi
+
+if [ -n $2 ]; then
+	TARGET=$2
 fi
 
 INPUT=$1
 echo Running 'gradlew clean' on gradlew projects...
 for c in $(jq '.[] | select(.build == "gradlew").versions[].dir' < "$INPUT"); do
-  ( cc=$(sed -e 's/^"//' -e 's/"$//' <<<"$c"); cd $cc; ./gradlew clean )
+	cc=$(sed -e 's/^"//' -e 's/"$//' <<<"$c")
+	if [ -z $TARGET ] || [ $TARGET == $cc ]; then
+    ( cd $cc; ./gradlew clean )
+	fi
 done
 
 echo Running 'mvn clean' on mvn projects...
 for c in $(jq '.[] | select(.build == "mvn").versions[].dir' < "$INPUT"); do
-  ( cc=$(sed -e 's/^"//' -e 's/"$//' <<<"$c"); cd $cc; mvn clean )
+	cc=$(sed -e 's/^"//' -e 's/"$//' <<<"$c")
+	if [ -z $TARGET ] || [ $TARGET == $cc ]; then
+    ( cd $cc; mvn clean )
+	fi
 done
 
